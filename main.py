c05e590af5dc878ed640fe282ec50e668d66535e
jecer <jecer1995@yandex.ru>
2023-03-03 17:45:58 +0300

    update

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-import PIL
+from pathlib import Path
 from PIL import Image
-import os.path
 
-path = './img'
-dirs = os.listdir(path)
-out = './img/resize'
-resize = int(input('Width: '))
+path = Path('./')
+resize = int(input('Enter resize size: '))
+out_dir = path / 'resized'
+out_dir.mkdir(exist_ok=True, parents=True)
 
-
-def crop():
-    for item in dirs:
-        fullpath = os.path.join(path, item)
-        outhome = os.path.join(out, item)
-        if os.path.isfile(fullpath):
-            img = Image.open(fullpath)
-            f, e = os.path.splitext(outhome)
-            size = resize
-            wpercent = (size / float(img.size[0]))
-            hsize = int((float(img.size[1]) * float(wpercent)))
-            img = img.resize((size, hsize), PIL.Image.ANTIALIAS)
-            img.save(f + '_Cropped.jpg', quality=100)
-
-
-crop()
+ignore = ['.py', '.exe']
+for item in path.iterdir():
+    if item.is_dir():
+        images_found = False
+        for subitem in item.iterdir():
+            if subitem.is_file() and subitem.suffix not in ignore and subitem.suffix.lower() in ['.jpg', '.png']:
+                images_found = True
+                image = Image.open(subitem)
+                max_dimension = max(image.size)
+                scale = resize / max_dimension
+                new_size = (int(image.size[0] * scale), int(image.size[1] * scale))
+                image = image.resize(new_size, Image.LANCZOS)
+                if subitem.suffix == '.jpg':
+                    image.save(out_dir / item.name / (subitem.stem + '.jpg'), quality=100)
+                elif subitem.suffix == '.png':
+                    image.save(out_dir / item.name / (subitem.stem + '.png'), quality=100)
+        if not images_found:
+            (out_dir / item.name).mkdir(exist_ok=True)
+    elif item.is_file() and item.suffix not in ignore and item.suffix.lower() in ['.jpg', '.png']:
+        image = Image.open(item)
+        max_dimension = max(image.size)
+        scale = resize / max_dimension
+        new_size = (int(image.size[0] * scale), int(image.size[1] * scale))
+        image = image.resize(new_size, Image.LANCZOS)
+        if item.suffix == '.jpg':
+            image.save(out_dir / (item.stem + '.jpg'), quality=100)
+        elif item.suffix == '.png':
+            image.save(out_dir / (item.stem + '.png'), quality=100)
